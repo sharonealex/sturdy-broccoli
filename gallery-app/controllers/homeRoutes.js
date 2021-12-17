bcca807898d0f36863ee7939b9443b99c0da8b6a
Sharon <sharonelizalex@gmail.com>
2021-12-17 14:02:52 +1100

    added painting handlebars

--- gallery-app/controllers/homeRoutes.js
+++ gallery-app/controllers/homeRoutes.js
@@ -24,4 +24,44 @@ router.get("/", async(req, res)=>{
     }
 });
 
+
+
+router.get("/gallery/:id", async(req, res)=>{
+    try{
+        const dbGalleryData = await Gallery.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Painting,
+                    attributes: [
+                        'id',
+                        'title',
+                        'artist',
+                        'exhibition_date',
+                        'filename',
+                        'description',
+                    ]
+                }
+            ]
+        });
+        const gallery = dbGalleryData.get({plain: true});
+        res.render('gallery', {gallery})
+    }catch(e){
+console.log(e)
+    };
+});
+
+router.get('/painting/:id', async (req, res) => {
+    try {
+      const dbPaintingData = await Painting.findByPk(req.params.id);
+  
+      const painting = dbPaintingData.get({ plain: true });
+  
+      res.render('painting', { painting });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+
 module.exports = router;
